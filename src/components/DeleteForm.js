df7075f4a972e3d95a28702e75c7e85e38371d6c
AGCH <anastasiyache@gmail.com>
2018-11-14 17:30:09 +0300

    added close button

--- src/components/DeleteForm.js
+++ src/components/DeleteForm.js
@@ -1,7 +1,8 @@
  
  import React, { Component } from 'react';
 import './css/deleteForm.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { observer, inject } from 'mobx-react';
 @inject("store")
 @observer
@@ -15,12 +16,11 @@ class DeleteForm extends Component {
     return (
       <div className="delete-form">
         <div className="close-button">
-        {/* <FontAwesomeIcon onClick={this.props.closeDeleteForm} icon="window-close" />*/}
+        <FontAwesomeIcon style={{ color: "#AAAAAA" }} className="fas" icon={faTimes} onClick={this.props.closeDeleteForm} />
         </div> 
         <div>Are you sure you want to delete this record? </div>
         <div className="div-button">
           <button onClick={this.deleteRecord}>YES</button>
-          <span> </span>
           <button onClick={this.props.closeDeleteForm}>NO</button>
         </div>
       </div>
